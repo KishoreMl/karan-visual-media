c279bbbe5f12f0d75860815ad4a18f08c800ec59
Kishore_ML <kishore_ml@trimble.com>
2025-10-18 19:29:55 +0530

    Header Component UI

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../assets/images/logo.webp';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+           <div className="logo">
+            <img src={logo} alt="logo" />
+           </div>
+            <div className="nav-bar">
+                <div className="nav-bar-item">Home</div>
+                <div className="nav-bar-item">Services</div>
+                <div className="nav-bar-item">About</div>
+                <div className="nav-bar-item">Contact</div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
