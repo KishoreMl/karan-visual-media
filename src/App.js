ad765c0078555edf3c9b12de7666bd65e47082e8
Kishore_ML <kishore_ml@trimble.com>
2025-10-29 18:05:09 +0530

    Home page styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header.jsx';
 // import Footer from './Components/Footer/Footer.jsx';
-import Content from './Components/Content.jsx';
+import Home from './Components/Home.jsx';
 import DescriptionCard from './Components/DescriptionCard/DescriptionCard.jsx';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Home />
       <DescriptionCard />
     </div>
   );
